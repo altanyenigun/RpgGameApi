347fc582047c0bab7fe3c2431fd4cea50f7d1745
altanyenigun <altanyenigun@gmail.com>
2023-09-10 18:02:33 +0300

    creating User Model &&  adding relation between Character & User Models

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -18,6 +18,7 @@ namespace RpgGameApi.Data
 
         public DbSet<Character> Characters => Set<Character>();
         // public DbSet<Character> Characters {get;set;} // Character objesinden oluşacak Characters tablosunu oluştur. Tablo ismini genelde üretilecek modelin çoğul hali yazılır.
+        public DbSet<User> Users => Set<User>();
 
     }
 }
