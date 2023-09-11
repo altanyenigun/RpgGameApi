3c79347dcb4805c81fa1ebf4dff353337caf62c6
altanyenigun <altanyenigun@gmail.com>
2023-09-11 19:27:17 +0300

    way to add data to database with migration on initial project start

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -19,5 +19,13 @@ namespace RpgGameApi.Data
         public DbSet<Character> Characters => Set<Character>();
         // public DbSet<Character> Characters {get;set;} // Character objesinden oluşacak Characters tablosunu oluştur. Tablo ismini genelde üretilecek modelin çoğul hali yazılır.
 
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     var c1 = new Character{Id=1,Name="Altan",HitPoints=10,Strength=10,Defense=10,Intelligence=10, Class = RpgClass.Knight};
+        //     var c2 = new Character{Id=2,Name="Memet",HitPoints=101,Strength=101,Defense=101,Intelligence=101, Class = RpgClass.Mage};
+
+        //     modelBuilder.Entity<Character>().HasData(c1,c2);
+        //     base.OnModelCreating(modelBuilder);
+        // }
     }
 }
---
+++ Migrations/20230911161806_SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RpgGameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "Id", "Class", "Defense", "HitPoints", "Intelligence", "Name", "Strength" },
+                values: new object[,]
+                {
+                    { 1, 1, 10, 10, 10, "Altan", 10 },
+                    { 2, 2, 101, 101, 101, "Memet", 101 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
